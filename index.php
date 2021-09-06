10df6df8057a8f216f0187060b338581c323dbec
Sam Wensko <swensko@mcc.edu>
2021-09-06 01:15:04 +0000

    bootstrap

--- index.php
+++ index.php
@@ -1,36 +1,68 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<div class="navbar">
-  <a href="/index.php">Home</a>
-  <a href="/mysqlstatus.php">MySQL Status</a>
-  <a href="/phpinfo.php">PHP Info</a>
-  <a href="/phpmyadmin">phpMyAdmin</a>
-</div>
+    <title>COMW-282 Home</title>
 
-<div class="main">
-  <h1>COMW-282 Dynamic Web</h1>
-  <div class="projectlist">
-    <a href="project1">Project 1: A Description of Project 1</a><br/>
-    <a href="project1">Project 1: A Description of Project 1</a><br/>
-    <a href="project1">Project 1: A Description of Project 1</a><br/>
-    <a href="project1">Project 1: A Description of Project 1</a><br/>
-    <a href="project1">Project 1: A Description of Project 1</a><br/>
-    <a href="project1">Project 1: A Description of Project 1</a><br/>
-    <a href="project1">Project 1: A Description of Project 1</a><br/>
-    <a href="project1">Project 1: A Description of Project 1</a><br/>
-    <a href="project1">Project 1: A Description of Project 1</a><br/>
-    <a href="project1">Project 1: A Description of Project 1</a><br/>
-    <a href="project1">Project 1: A Description of Project 1</a><br/>
-    <a href="project1">Project 1: A Description of Project 1</a><br/>
-    <a href="project1">Project 1: A Description of Project 1</a><br/>
-  </div>
-</div>
+    <!-- Bootstrap CSS and Theme -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap-dark.css" rel="stylesheet">
 
-</body>
+  </head>
+  <body class="d-flex flex-column h-100">
+        
+    <header>
+      <!-- Fixed navbar -->
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">COMW-282</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+              <li class="nav-item">
+                <a class="nav-link" href="/mysqlstatus.php">MySQL Status</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/phpinfo.php">PHP Info</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/phpmyadmin">phpMyAdmin</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <!-- Begin page content -->
+    <main class="flex-shrink-0">
+      <div class="container" style="padding: 60px 15px 60px;">
+        <h1 class="mt-5">COMW-282 Dynamic Web</h1>
+        <p class="lead">Learning PHP, MySQL, and Javascript</p>
+        <ul>
+          <li><a href="/assignments/project1/">Project 1: A Description of Project 1</a><br/></li>
+          <li><a href="/css">Project 1: A Description of Project 1</a><br/></li>
+          <li><a href="/assignments/project1/">Project 1: A Description of Project 1</a><br/></li>
+          <li><a href="/assignments/project1/">Project 1: A Description of Project 1</a><br/></li>
+          <li><a href="/assignments/project1/">Project 1: A Description of Project 1</a><br/></li>
+          <li><a href="/assignments/project1/">Project 1: A Description of Project 1</a><br/></li>
+          <li><a href="/assignments/project1/">Project 1: A Description of Project 1</a><br/></li>
+          <li><a href="/assignments/project1/">Project 1: A Description of Project 1</a><br/></li>
+        </ul>
+      </div>
+    </main>
+
+    <footer class="footer fixed-bottom mt-auto py-3 bg-light">
+      <div class="container">
+        <span class="lead">This site was created for educational purposes only.</span>
+      </div>
+    </footer>
+
+    <!-- Bootstrap Bundled Javascript -->
+    <script src="/js/bootstrap.bundle.min.js"></script>
+  </body>
 </html>
--- phpinfo.php
+++ phpinfo.php
@@ -1,36 +1,73 @@
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!DOCTYPE html>
-<html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
+    <title>PHP Info</title>
 
-<div class="navbar">
-  <a href="/index.php">Home</a>
-  <a href="/mysqlstatus.php">MySQL Status</a>
-  <a href="/phpinfo.php">PHP Info</a>
-  <a href="/phpmyadmin">phpMyAdmin</a>
-</div>
+    <!-- Bootstrap CSS and Theme -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap-dark.css" rel="stylesheet">
 
-<div class="main">
-    <div>
-    
-    <?php
+  </head>
+  <body class="d-flex flex-column h-100">
+        
+  <header>
+      <!-- Fixed navbar -->
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">COMW-282</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+              <li class="nav-item">
+                <a class="nav-link" href="/mysqlstatus.php">MySQL Status</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/phpinfo.php">PHP Info</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/phpmyadmin">phpMyAdmin</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
 
-        ob_start () ;
-        phpinfo () ;
-        $pinfo = ob_get_contents () ;
-        ob_end_clean () ;
+    <!-- Begin page content -->
+    <main class="flex-shrink-0">
+      <div class="container" style="padding: 60px 15px 60px;">
 
-        // the name attribute "module_Zend Optimizer" of an anker-tag is not xhtml valide, so replace it with "module_Zend_Optimizer"
-        echo ( str_replace ( "module_Zend Optimizer", "module_Zend_Optimizer", preg_replace ( '%^.*<body>(.*)</body>.*$%ms', '$1', $pinfo ) ) ) ;
+      <div class="mt-5 text-truncate">
+      
+        <?php
 
-    ?>
+            ob_start () ;
+            phpinfo () ;
+            $pinfo = ob_get_contents () ;
+            ob_end_clean () ;
 
-    </div>
-</div>
+            // the name attribute "module_Zend Optimizer" of an anker-tag is not xhtml valide, so replace it with "module_Zend_Optimizer"
+            echo ( str_replace ( "module_Zend Optimizer", "module_Zend_Optimizer", preg_replace ( '%^.*<body>(.*)</body>.*$%ms', '$1', $pinfo ) ) ) ;
 
-</body>
+        ?>
+
+      </div>
+
+      </div>
+    </main>
+
+    <footer class="footer fixed-bottom mt-auto py-3 bg-light">
+      <div class="container">
+        <span class="lead">This site was created for educational purposes only.</span>
+      </div>
+    </footer>
+
+    <!-- Bootstrap Bundled Javascript -->
+    <script src="/js/bootstrap.bundle.min.js"></script>
+  </body>
 </html>
